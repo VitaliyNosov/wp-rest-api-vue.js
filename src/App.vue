72a4a7a7c71911d464a31b10f0d0da0d0db48f00
VitaliyNosov <webdevelorestudio2017@gmail.com>
2022-03-21 20:30:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/pupok">Pupok</router-link> |
+    <router-link to="/zalupok">Zalupok</router-link> |
+    <router-link to="/wordpress-page">Word Press-Page</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+
+.wordpress-page-style{
+  width: 100%;
+  height: 86.5vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  background-color: #42b983;
+  color: #fff;
+  
+}
+
+</style>
+
+
---
+++ src/views/WordPressPageView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about wordpress-page-style">
+    <h1>This is an about WordPress Page</h1>
+  </div>
+</template>
